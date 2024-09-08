1993e111d67cd07cbdf2c4e2e8ad8a9556cb7871
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-08 16:05:52 +0100

    update App.tsx and Generator.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Generator from "./components/Generator";
 import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <>
-      <SideBar />
-    </>
+    <div className="flex">
+      <div className="basis-1/2">
+        <SideBar />
+      </div>
+      <div className="basis-1/2">
+        <Generator />
+      </div>
+    </div>
   );
 }
 export default App;
--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import QRCode from "react-qr-code";
 function Generator() {
+  const [text, setText] = useState("");
+  function generateQR(e) {
+    setText();
+  }
+  function handleChange(e) {
+    setText(e.target.value);
+  }
   return (
     <div>
-      <QRCode value="tutorend.com" />
+      <QRCode value={text} />
+      <div className="input-here">
+        <p>Enter your text here</p>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <button>Generate</button>
+      </div>
     </div>
   );
 }
